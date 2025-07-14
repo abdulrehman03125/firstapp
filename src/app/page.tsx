69e265b56cf3bfa51c814b60a1bc4b362d3becdf
Abdul Rahman <chrahman0312@gmail.com>
2025-07-14 22:51:38 +0500

    update
    remove this extere

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import { AnimatedPinDemo } from "@/components/AnimatedPinDemo/AnimatedPinDemo";
 import { AnimatedTooltipPreview } from "@/components/AnimatedTooltipPreview/AnimatedTooltipPreview";
-//import { AppleCardsCarouselDemo } from "@/components/AppleCardsCarouselDemo/AppleCardsCarouselDemo";
-
 import Featurescard from "@/components/feturescard/Featurescard";
 import Hero_Section from "@/components/heresection/Hero_Section";
 import { InfiniteMovingCardsDemo } from "@/components/InfiniteMovingCards/InfiniteMovingCardsDemo";
 import { StickyScrollRevealDemo } from "@/components/StickyScroll/StickyScrollRevealDemo";
- // import { SpotlightPreview } from "@/components/SpotlightNew/SpotlightPreview";
 import Footer from "@/components/Footer";
 
 
@@ -14,14 +11,12 @@ export default function Home() {
   return (
    <div className="min-h-screen ">
     <Hero_Section/>
-    {/* <AppleCardsCarouselDemo/> */}
     <Featurescard/>
     <StickyScrollRevealDemo/>
     <InfiniteMovingCardsDemo/>
     <AnimatedPinDemo/>
     <AnimatedTooltipPreview/>
     <Footer/>
-    {/* <SpotlightPreview/> */}
    </div>
    
   );
